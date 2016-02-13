75c6f6f805b291ccc4d653a08ca286d7d4537aa7
Richard Kirby <rk755@york.ac.uk>
2016-02-13 01:49:34 +0000

    Add AssemblyInfo files for projects.

---
+++ src/XogoEngine.OpenGL.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("XogoEngine.OpenGL.Test")]
+[assembly: AssemblyDescription("XogoEngine's OpenGL Layer Unit Tests")]
+[assembly: AssemblyCopyright("Richard Kirby")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
---
+++ src/XogoEngine.OpenGL/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("XogoEngine.OpenGL")]
+[assembly: AssemblyDescription("XogoEngine's OpenGL Layer")]
+[assembly: AssemblyCopyright("Richard Kirby")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
