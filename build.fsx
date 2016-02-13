95080bf1d687fee07e5c33f0bdeb1ccf1060420c
Richard Kirby <rk755@york.ac.uk>
2016-02-13 00:23:56 +0000

    Add FAKE build script.

---
+++ build.fsx
@@ -0,0 +1,80 @@
+#r @"packages/Build/FAKE/tools.FakeLib.dll"
+
+open Fake
+
+let buildDir     = "./build/"
+let testDir      = "./test/"
+let reportDir    = "./reports/"
+let historyDir   = "./history/"
+let coverageDir  = reportDir + "coverage/"
+let coverageFile = coverageDir + "code-coverage.xml"
+
+let xogoGlProject     = "src/XogoEngine.OpenGL/XogoEngine.OpenGL.csproj"
+let xogoGlTestProject = "src/XogoEngine.OpenGL.Test/XogoEngine.OpenGL.Test.csproj"
+
+let coreProjects = [xogoGlProject]
+let testProjects = [xogoGlTestProject]
+
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir; testDir; reportDir; coverageDir]
+)
+
+Target "CreateDirs" (fun _ ->
+    CreateDir reportDir
+    CreateDir coverageDir
+)
+
+Target "Build_Core_Assemblies" (fun _ ->
+    MSBuildDebug buildDir "Build" coreProjects
+        |> Log "Build Core Assemblies Ouput:"
+)
+
+Target "Build_Test_Assemblies" (fun _ ->
+    MSBuildDebug testDir "Build" testProjects
+        |> Log "Build Test Assemblies Output:"
+)
+
+Target "UnitTest" (fun _ ->
+    !! (testDir + "XogoEngine.*Test.dll")
+        |> NUnit (fun p ->
+            {p with
+                DisableShadowCopy = true
+                ShowLabels = false
+            }
+        )
+)
+
+Target "UnitTest_WithCoverage" (fun _ ->
+    "/noshadow " + testDir + "XogoEngine.OpenGL.Test.dll "
+        |> OpenCover (fun p ->
+            {p with
+                ExePath = "packages/tools/OpenCover/tools/OpenCover.Console.exe"
+                TestRunnerExePath = "packages/tools/NUnit.Runners/tools/nunit-console.exe"
+                Output = coverageFile
+                Filter = "+[XogoEngine*]* -[*.Test*]*"
+            }
+        )
+)
+
+Target "GenerateCoverageReport" (fun _ ->
+    [coverageFile]
+        |> ReportGenerator (fun p ->
+            {p with
+                ExePath = "packages/tools/ReportGenerator/tools/ReportGenerator.exe"
+                TargetDir = reportDir
+            }
+        )
+)
+
+Target "Default" DoNothing
+
+"Clean"
+    ==> "CreateDirs"
+    ==> "Build_Core_Assemblies"
+    ==> "Build_Test_Assemblies"
+    =?> ("UnitTest", isMono)
+    =?> ("UnitTest_WithCoverage", not isMono)
+    =?> ("GenerateCoverageReport", not isMono)
+    ==> "Default"
+
+RunTargetOrDefault "Default"
