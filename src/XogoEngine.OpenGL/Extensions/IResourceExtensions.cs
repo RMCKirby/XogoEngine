7b110eb843768080c24ed8a646f9f07df4ee5332
Richard Kirby <rk755@york.ac.uk>
2016-02-15 16:00:37 +0000

    Added Extensions folder and IResource extensions.

---
+++ src/XogoEngine.OpenGL/Extensions/IResourceExtensions.cs
@@ -0,0 +1,13 @@
+namespace XogoEngine.OpenGL.Extensions
+{
+    public static class IResourceExtensions
+    {
+        public static void ThrowIfDisposed<T>(this IResource<T> instance) where T : struct
+        {
+            if (instance.IsDisposed)
+            {
+                throw new System.ObjectDisposedException(instance.GetType().FullName);
+            }
+        }
+    }
+}
