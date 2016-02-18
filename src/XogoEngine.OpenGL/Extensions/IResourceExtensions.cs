82108bda72aeebed4775158d1c954164c0cb56da
Richard Kirby <rk755@york.ac.uk>
2016-02-18 23:10:56 +0000

    Added tests for IResource extensions.

--- src/XogoEngine.OpenGL/Extensions/IResourceExtensions.cs
+++ src/XogoEngine.OpenGL/Extensions/IResourceExtensions.cs
@@ -2,9 +2,9 @@ namespace XogoEngine.OpenGL.Extensions
 {
     public static class IResourceExtensions
     {
-        internal static void ThrowIfDisposed<T>(this IResource<T> instance) where T : struct
+        public static void ThrowIfDisposed<T>(this IResource<T> instance) where T : struct
         {
-            if (instance.IsDisposed)
+            if (instance?.IsDisposed ?? false)
             {
                 throw new System.ObjectDisposedException(instance.GetType().FullName);
             }
