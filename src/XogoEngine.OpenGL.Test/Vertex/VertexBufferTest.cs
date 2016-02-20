b433a735053ea107cbaf7e72ac3f7ce9eb2af30a
Richard Kirby <rk755@york.ac.uk>
2016-02-20 17:42:55 +0000

    Implementing VertexBuffer constructor.

--- src/XogoEngine.OpenGL.Test/Vertex/VertexBufferTest.cs
+++ src/XogoEngine.OpenGL.Test/Vertex/VertexBufferTest.cs
@@ -17,7 +17,24 @@ namespace XogoEngine.OpenGL.Test.Vertex
         public void SetUp()
         {
             adapter = new Mock<IBufferAdapter>();
+            adapter.Setup(a => a.GenBuffer())
+                   .Returns(1);
             buffer = new VertexBuffer<int>(adapter.Object);
+        }
+
+        [Test]
+        public void AdapterGenBuffer_isInvokedOnlyOnce_OnConstruction()
+        {
+            adapter.Verify(a => a.GenBuffer(), Times.Once);
+        }
+
+        [Test]
+        public void Constructor_CorrectlyInitialises_Instance()
+        {
+            buffer.ShouldSatisfyAllConditions(
+                () => buffer.Handle.ShouldBe(1),
+                () => buffer.IsDisposed.ShouldBe(false)
+            );
         }
     }
 }
--- src/XogoEngine.OpenGL/Vertex/VertexBuffer.cs
+++ src/XogoEngine.OpenGL/Vertex/VertexBuffer.cs
@@ -3,13 +3,25 @@ using XogoEngine.OpenGL.Adapters;
 
 namespace XogoEngine.OpenGL.Vertex
 {
-    public sealed class VertexBuffer<T> where T : struct
+    public sealed class VertexBuffer<T> : IResource<int> where T : struct
     {
+        private int handle;
         private readonly IBufferAdapter adapter;
+
+        private bool isDisposed = false;
 
         public VertexBuffer(IBufferAdapter adapter)
         {
             this.adapter = adapter;
+            this.handle = adapter.GenBuffer();
+        }
+
+        public int Handle { get { return handle; } }
+        public bool IsDisposed { get { return isDisposed; } }
+
+        public void Dispose()
+        {
+            
         }
     }
 }
