e6113ad420f7adae8647a77ff927b06414351302
Richard Kirby <rk755@york.ac.uk>
2016-03-02 18:32:14 +0000

    Implemented VertexPositionColourTexture.

--- src/XogoEngine.OpenGL.Test/Vertex/VertexPositionColourTextureTest.cs
+++ src/XogoEngine.OpenGL.Test/Vertex/VertexPositionColourTextureTest.cs
@@ -38,5 +38,48 @@ namespace XogoEngine.Test.OpenGL.Vertex
                 () => vertex.TextureCoordinate.ShouldBe(textureCoordinate)
             );
         }
+
+        [Test]
+        public void VertexStride_ShouldBe_ExpectedSize()
+        {
+            vertex.Declaration.Stride.ShouldBe(8 * sizeof(float));
+        }
+
+        [Test]
+        public void VertexElements_ShouldContain_ExpectedElements()
+        {
+            vertex.Declaration.Elements.Length.ShouldBe(3);
+            vertex.Declaration.Elements.ShouldContain(e => e.Usage == "position");
+            vertex.Declaration.Elements.ShouldContain(e => e.Usage == "colour");
+            vertex.Declaration.Elements.ShouldContain(e => e.Usage == "texCoord");
+        }
+
+        [Test]
+        public void VertexElements_ShouldHave_ExpectedProperties()
+        {
+            var position = vertex.Declaration.Elements.Single(e => e.Usage == "position");
+            var colour = vertex.Declaration.Elements.Single(e => e.Usage == "colour");
+            var textureCoordinate = vertex.Declaration.Elements.Single(e => e.Usage == "texCoord");
+
+            var pointerType = VertexAttribPointerType.Float;
+            AssertVertexElementProperties(position, 0, pointerType, 2, false);
+            AssertVertexElementProperties(colour, 8, pointerType, 4, false);
+            AssertVertexElementProperties(textureCoordinate, 24, pointerType, 2, false);
+        }
+
+        private void AssertVertexElementProperties(
+            VertexElement element,
+            int offset,
+            VertexAttribPointerType type,
+            int numberOfComponents,
+            bool normalised)
+        {
+            element.ShouldSatisfyAllConditions(
+                () => element.Offset.ShouldBe(offset),
+                () => element.PointerType.ShouldBe(type),
+                () => element.NumberOfComponents.ShouldBe(numberOfComponents),
+                () => element.Normalised.ShouldBe(normalised)
+            );
+        }
     }
 }
--- src/XogoEngine.OpenGL/Vertex/VertexPositionColourTexture.cs
+++ src/XogoEngine.OpenGL/Vertex/VertexPositionColourTexture.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 namespace XogoEngine.OpenGL.Vertex
 {
     [StructLayout(LayoutKind.Sequential)]
-    public struct VertexPositionColourTexture
+    public struct VertexPositionColourTexture : IVertexDeclarable
     {
         public VertexPositionColourTexture(Vector2 position, Vector4 colour, Vector2 textureCoordinate)
         {
@@ -17,5 +17,27 @@ namespace XogoEngine.OpenGL.Vertex
         public Vector2 Position { get; }
         public Vector4 Colour { get; }
         public Vector2 TextureCoordinate { get; }
+
+        public IVertexDeclaration Declaration
+        {
+            get
+            {
+                return declaration;
+            }
+        }
+
+        private static VertexDeclaration declaration;
+
+        static VertexPositionColourTexture()
+        {
+            var stride = Marshal.SizeOf(default(VertexPositionColourTexture));
+            var elements = new VertexElement[]
+            {
+                new VertexElement(0, VertexElementUsage.Position, VertexAttribPointerType.Float, 2, false),
+                new VertexElement(8, VertexElementUsage.Colour, VertexAttribPointerType.Float, 4, false),
+                new VertexElement(24, VertexElementUsage.TexCoord, VertexAttribPointerType.Float, 2, false)
+            };
+            declaration = new VertexDeclaration(stride, elements);
+        }
     }
 }
