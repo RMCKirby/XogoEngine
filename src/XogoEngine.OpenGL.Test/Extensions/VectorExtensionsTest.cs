3fa717ac5341bbf774659b07b588a71b241c5e52
Richard Kirby <rk755@york.ac.uk>
2016-03-02 23:43:51 +0000

    Implemented fix for Vector2/3/4 GetHashCode.

--- src/XogoEngine.OpenGL.Test/Extensions/VectorExtensionsTest.cs
+++ src/XogoEngine.OpenGL.Test/Extensions/VectorExtensionsTest.cs
@@ -12,7 +12,7 @@ namespace XogoEngine.OpenGL.Test.Extensions
         [Test, TestCaseSource(nameof(UnequalVector2s))]
         public void Hashcodes_AreNotEqual_ForUnequalVector2s(Vector2 left, Vector2 right)
         {
-            left.GetHashCode().ShouldNotBe(right.GetHashCode());
+            left.GetFixedHashCode().ShouldNotBe(right.GetFixedHashCode());
         }
 
         private IEnumerable<TestCaseData> UnequalVector2s
@@ -21,6 +21,43 @@ namespace XogoEngine.OpenGL.Test.Extensions
             {
                 yield return new TestCaseData(Vector2.Zero, Vector2.One);
                 yield return new TestCaseData(Vector2.UnitX, Vector2.UnitY);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(UnequalVector3s))]
+        public void HashCodes_AreNotEqual_ForUnequalVector3s(Vector3 left, Vector3 right)
+        {
+            left.GetFixedHashCode().ShouldNotBe(right.GetFixedHashCode());
+        }
+
+        private IEnumerable<TestCaseData> UnequalVector3s
+        {
+            get
+            {
+                yield return new TestCaseData(Vector3.Zero, Vector3.One);
+                yield return new TestCaseData(Vector3.UnitX, Vector3.UnitY);
+                yield return new TestCaseData(Vector3.UnitX, Vector3.UnitZ);
+                yield return new TestCaseData(Vector3.UnitY, Vector3.UnitZ);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(UnequalVector4s))]
+        public void HashCodes_AreNotEqual_ForUnequalVector4s(Vector4 left, Vector4 right)
+        {
+            left.GetFixedHashCode().ShouldNotBe(right.GetFixedHashCode());
+        }
+
+        private IEnumerable<TestCaseData> UnequalVector4s
+        {
+            get
+            {
+                yield return new TestCaseData(Vector4.Zero, Vector4.One);
+                yield return new TestCaseData(Vector4.UnitX, Vector4.UnitY);
+                yield return new TestCaseData(Vector4.UnitX, Vector4.UnitZ);
+                yield return new TestCaseData(Vector4.UnitX, Vector4.UnitW);
+                yield return new TestCaseData(Vector4.UnitY, Vector4.UnitZ);
+                yield return new TestCaseData(Vector4.UnitY, Vector4.UnitW);
+                yield return new TestCaseData(Vector4.UnitZ, Vector4.UnitW);
             }
         }
     }
--- src/XogoEngine.OpenGL/Extensions/VectorExtensions.cs
+++ src/XogoEngine.OpenGL/Extensions/VectorExtensions.cs
@@ -2,6 +2,8 @@ using OpenTK;
 
 namespace XogoEngine.OpenGL.Extensions
 {
+    /* FIX for OpenTK Vector types GetHashCode implementation
+     * see: https://github.com/opentk/opentk/issues/355 */
     public static class VectorExtensions
     {
         public static int GetFixedHashCode(this Vector2 vector)
@@ -11,6 +13,31 @@ namespace XogoEngine.OpenGL.Extensions
                 int hash = 17;
                 hash = hash * 23 + vector.X.GetHashCode();
                 hash = hash * 23 + vector.Y.GetHashCode();
+                return hash;
+            }
+        }
+
+        public static int GetFixedHashCode(this Vector3 vector)
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + vector.X.GetHashCode();
+                hash = hash * 23 + vector.Y.GetHashCode();
+                hash = hash * 23 + vector.Z.GetHashCode();
+                return hash;
+            }
+        }
+
+        public static int GetFixedHashCode(this Vector4 vector)
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + vector.X.GetHashCode();
+                hash = hash * 23 + vector.Y.GetHashCode();
+                hash = hash * 23 + vector.Z.GetHashCode();
+                hash = hash * 23 + vector.W.GetHashCode();
                 return hash;
             }
         }
