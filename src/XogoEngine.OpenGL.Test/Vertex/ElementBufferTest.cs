fbe7a832a69f92533dd223eb9093afb56e429793
Richard Kirby <rk755@york.ac.uk>
2016-03-03 21:26:09 +0000

    Ensure ElementBuffer is correctly instantiated.

--- src/XogoEngine.OpenGL.Test/Vertex/ElementBufferTest.cs
+++ src/XogoEngine.OpenGL.Test/Vertex/ElementBufferTest.cs
@@ -1,5 +1,8 @@
+using Moq;
 using NUnit.Framework;
 using Shouldly;
+using OpenTK.Graphics.OpenGL4;
+using XogoEngine.OpenGL.Adapters;
 using XogoEngine.OpenGL.Vertex;
 
 namespace XogoEngine.OpenGL.Test.Vertex
@@ -7,6 +10,27 @@ namespace XogoEngine.OpenGL.Test.Vertex
     [TestFixture]
     internal sealed class ElementBufferTest
     {
+        private ElementBuffer<ushort> elementBuffer;
+        private Mock<IBufferAdapter> adapter;
 
+        [SetUp]
+        public void SetUp()
+        {
+            adapter = new Mock<IBufferAdapter>();
+            adapter.Setup(a => a.GenBuffer())
+                   .Returns(1);
+
+            elementBuffer = new ElementBuffer<ushort>(adapter.Object);
+        }
+
+        [Test]
+        public void Constructor_CorrectlyInitialises_Instance()
+        {
+            elementBuffer.ShouldSatisfyAllConditions(
+                () => elementBuffer.Handle.ShouldBe(1),
+                () => elementBuffer.IsDisposed.ShouldBeFalse(),
+                () => elementBuffer.Target.ShouldBe(BufferTarget.ElementArrayBuffer)
+            );
+        }
     }
 }
--- src/XogoEngine.OpenGL/Vertex/ElementBuffer.cs
+++ src/XogoEngine.OpenGL/Vertex/ElementBuffer.cs
@@ -1,7 +1,29 @@
+using OpenTK.Graphics.OpenGL4;
+using XogoEngine.OpenGL.Adapters;
+using XogoEngine.OpenGL.Extensions;
+
 namespace XogoEngine.OpenGL.Vertex
 {
-    public sealed class ElementBuffer
+    public sealed class ElementBuffer<T> : IResource<int> where T : struct
     {
-        
+        private int handle;
+        private readonly IBufferAdapter adapter;
+
+        private bool isDisposed = false;
+
+        public ElementBuffer(IBufferAdapter adapter)
+        {
+            this.adapter = adapter;
+            this.handle = adapter.GenBuffer();
+        }
+
+        public int Handle { get { return handle; } }
+        public BufferTarget Target { get { return BufferTarget.ElementArrayBuffer; } }
+        public bool IsDisposed { get { return isDisposed; } }
+
+        public void Dispose()
+        {
+
+        }
     }
 }
