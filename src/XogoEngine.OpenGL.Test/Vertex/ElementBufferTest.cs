a4c6128d8e4d77f3b0a48d5547064ef08e702d84
Richard Kirby <rk755@york.ac.uk>
2016-03-03 21:09:08 +0000

    Beginning implementation of ElementBuffer.

---
+++ src/XogoEngine.OpenGL.Test/Vertex/ElementBufferTest.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using Shouldly;
+using XogoEngine.OpenGL.Vertex;
+
+namespace XogoEngine.OpenGL.Test.Vertex
+{
+    [TestFixture]
+    internal sealed class ElementBufferTest
+    {
+
+    }
+}
---
+++ src/XogoEngine.OpenGL/Vertex/ElementBuffer.cs
@@ -0,0 +1,7 @@
+namespace XogoEngine.OpenGL.Vertex
+{
+    public sealed class ElementBuffer
+    {
+        
+    }
+}
