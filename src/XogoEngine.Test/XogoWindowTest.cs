d33cd84e78fea114304f848261e57a3d64a652df
Richard Kirby <rk755@york.ac.uk>
2016-03-04 13:23:49 +0000

    Adding Core engine basic structure.

---
+++ src/XogoEngine.Test/XogoWindowTest.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+using Shouldly;
+
+namespace XogoEngine.Test
+{
+    [TestFixture]
+    internal sealed class XogoWindowTest
+    {
+        
+    }
+}
---
+++ src/XogoEngine/XogoWindow.cs
@@ -0,0 +1,7 @@
+namespace XogoEngine
+{
+    public class XogoWindow
+    {
+        
+    }
+}
