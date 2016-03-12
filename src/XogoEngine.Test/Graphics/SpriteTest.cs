6e5f9704c077f06c0418ad490494b09a63f5a9b2
Richard Kirby <rk755@york.ac.uk>
2016-03-12 20:27:56 +0000

    Beginning Sprite class (at long last).

---
+++ src/XogoEngine.Test/Graphics/SpriteTest.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using Shouldly;
+using XogoEngine.Graphics;
+
+namespace XogoEngine.Test.Graphics
+{
+    [TestFixture]
+    internal sealed class SpriteTest
+    {
+        
+    }
+}
