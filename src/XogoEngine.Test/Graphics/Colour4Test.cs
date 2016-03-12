a5bb8edf9e3aae9a5f9d73b47d49835597c06685
Richard Kirby <rk755@york.ac.uk>
2016-03-12 20:02:56 +0000

    Implemented IEquatable on Colour4 type.

--- src/XogoEngine.Test/Graphics/Colour4Test.cs
+++ src/XogoEngine.Test/Graphics/Colour4Test.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using Shouldly;
+using System.Collections.Generic;
 using XogoEngine.Graphics;
 
 namespace XogoEngine.Test.Graphics
@@ -7,6 +8,99 @@ namespace XogoEngine.Test.Graphics
     [TestFixture]
     internal sealed class Colour4Test
     {
-        
+        private Colour4 colour;
+        private static float red = 2;
+        private static float green = 3;
+        private static float blue = 4;
+
+        [SetUp]
+        public void SetUp()
+        {
+            colour = new Colour4(red, green, blue);
+        }
+
+        [Test]
+        public void ColourComponents_ShouldBeCorrectlySet_AfterConstruction()
+        {
+            colour.ShouldSatisfyAllConditions(
+                () => colour.R.ShouldBe(2),
+                () => colour.G.ShouldBe(3),
+                () => colour.B.ShouldBe(4),
+                () => colour.A.ShouldBe(1)
+            );
+        }
+
+        [Test]
+        public void ObjectEquals_ReturnsFalse_OnNullObject()
+        {
+            colour.Equals(null).ShouldBeFalse();
+        }
+
+        [Test, TestCaseSource(nameof(UnequalColours))]
+        public void ObjectEquals_ReturnsFalse_OnUnequalColours(object other)
+        {
+            colour.Equals(other).ShouldBeFalse();
+        }
+
+        [Test, TestCaseSource(nameof(UnequalColours))]
+        public void ValueEquals_ReturnsFalse_OnUnequalColours(Colour4 other)
+        {
+            colour.Equals(other).ShouldBeFalse();
+        }
+
+        [Test, TestCaseSource(nameof(EqualColour))]
+        public void ObjectEquals_ReturnsTrue_OnEqualColour(object other)
+        {
+            colour.Equals(other).ShouldBeTrue();
+        }
+
+        [Test, TestCaseSource(nameof(EqualColour))]
+        public void ValueEquals_ReturnsTrue_OnEqualColour(Colour4 other)
+        {
+            colour.Equals(other).ShouldBeTrue();
+        }
+
+        [Test, TestCaseSource(nameof(UnequalColours))]
+        public void EqualsOperator_ReturnsFalse_OnUnequalColours(Colour4 other)
+        {
+            (colour == other).ShouldBeFalse();
+        }
+
+        [Test, TestCaseSource(nameof(EqualColour))]
+        public void EqualsOperator_ReturnsTrue_OnEqualColour(Colour4 other)
+        {
+            (colour == other).ShouldBeTrue();
+        }
+
+        [Test, TestCaseSource(nameof(UnequalColours))]
+        public void NotEqualsOperator_ReturnsTrue_OnUnequalColours(Colour4 other)
+        {
+            (colour != other).ShouldBeTrue();
+        }
+
+        [Test, TestCaseSource(nameof(EqualColour))]
+        public void NotEqualsOperator_ReturnsFalse_OnEqualColour(Colour4 other)
+        {
+            (colour != other).ShouldBeFalse();
+        }
+
+        private IEnumerable<TestCaseData> UnequalColours
+        {
+            get
+            {
+                yield return new TestCaseData(new Colour4(200, green, blue));
+                yield return new TestCaseData(new Colour4(red, 200, blue));
+                yield return new TestCaseData(new Colour4(red, green, 200));
+                yield return new TestCaseData(new Colour4(red, green, blue, 200));
+            }
+        }
+
+        private static IEnumerable<TestCaseData> EqualColour
+        {
+            get
+            {
+                yield return new TestCaseData(new Colour4(red, green, blue));
+            }
+        }
     }
 }
--- src/XogoEngine/Graphics/Colour4.cs
+++ src/XogoEngine/Graphics/Colour4.cs
@@ -1,10 +1,47 @@
+using System;
 using System.Runtime.InteropServices;
 
 namespace XogoEngine.Graphics
 {
     [StructLayout(LayoutKind.Sequential)]
-    public struct Colour4
+    public struct Colour4 : IEquatable<Colour4>
     {
-        
+        public Colour4(float red, float green, float blue)
+            : this(red, green, blue, 1)
+        {
+        }
+
+        public Colour4(float red, float green, float blue, float alpha)
+        {
+            R = red;
+            G = green;
+            B = blue;
+            A = alpha;
+        }
+
+        public float R { get; }
+        public float G { get; }
+        public float B { get; }
+        public float A { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Colour4))
+            {
+                return false;
+            }
+            return Equals((Colour4)obj);
+        }
+
+        public bool Equals(Colour4 other)
+        {
+            return R == other.R &&
+                   G == other.G &&
+                   B == other.B &&
+                   A == other.A;
+        }
+
+        public static bool operator ==(Colour4 left, Colour4 right) => left.Equals(right);
+        public static bool operator !=(Colour4 left, Colour4 right) => !left.Equals(right);
     }
 }
