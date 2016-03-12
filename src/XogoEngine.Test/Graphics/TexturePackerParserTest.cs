23b8e3c4e01d3172015fafc2e356c3370eaaa8f0
Richard Kirby <rk755@york.ac.uk>
2016-03-12 13:32:03 +0000

    Beginning TexturePackerParser implementation.

---
+++ src/XogoEngine.Test/Graphics/TexturePackerParserTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using XogoEngine.Graphics;
+
+namespace XogoEngine.Test.Graphics
+{
+    [TestFixture]
+    internal sealed class TexturePackerParserTest
+    {
+        private TexturePackerParser parser;
+
+        private static string texturePath = "assets/spritesheet.png";
+        private static string dataFilePath = "assets/spritesheet-data.png";
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+    }
+}
---
+++ src/XogoEngine/Graphics/TexturePackerParser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XogoEngine.Graphics
+{
+    internal class TexturePackerParser
+    {
+        public void Parse(string dataFilePath)
+        {
+            
+        }
+    }
+}
