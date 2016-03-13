295e561e630d3853a799a1b48dafe534d987b910
Richard Kirby <rk755@york.ac.uk>
2016-03-13 14:53:10 +0000

    Initialise Sprite vertices.

--- src/XogoEngine.Test/Graphics/SpriteTest.cs
+++ src/XogoEngine.Test/Graphics/SpriteTest.cs
@@ -1,13 +1,28 @@
 using NUnit.Framework;
 using Shouldly;
 using System;
+using System.Collections.Generic;
+using OpenTK;
 using XogoEngine.Graphics;
+using XogoEngine.OpenGL.Vertex;
 
 namespace XogoEngine.Test.Graphics
 {
     [TestFixture]
     internal sealed class SpriteTest
     {
+        private Sprite sprite;
+        private static TextureRegion textureRegion = new TextureRegion(2, 2, 15, 20);
+        private static Vector4 colour = new Vector4(
+            Colour4.White.R, Colour4.White.G, Colour4.White.B, Colour4.White.A
+        );
+
+        [SetUp]
+        public void SetUp()
+        {
+            sprite = new Sprite(textureRegion, 40, 50);
+        }
+
         [Test]
         public void Constructor_ThrowsArgumentNullException_OnNullTextureRegion()
         {
@@ -19,15 +34,55 @@ namespace XogoEngine.Test.Graphics
         [Test]
         public void Constructor_CorrectlyInitialises_Instance()
         {
-            var region = new TextureRegion(2, 2, 15, 20);
-            var sprite = new Sprite(region, 40, 50);
-
             sprite.ShouldSatisfyAllConditions(
                 () => sprite.X.ShouldBe(40),
                 () => sprite.Y.ShouldBe(50),
                 () => sprite.Width.ShouldBe(15),
-                () => sprite.Height.ShouldBe(20)
+                () => sprite.Height.ShouldBe(20),
+                () => sprite.Colour.ShouldBe(Colour4.White)
             );
+        }
+
+        [Test, TestCaseSource(nameof(ExpectedVertices))]
+        public void Vertices_AreCreatedAsExpected_OnConstruction(VertexPositionColourTexture expected, int index)
+        {
+            sprite.Vertices.Length.ShouldBe(4);
+            sprite.Vertices[index].ShouldBe(expected);
+        }
+
+        private IEnumerable<TestCaseData> ExpectedVertices
+        {
+            get
+            {
+                yield return new TestCaseData(
+                    new VertexPositionColourTexture(
+                        new Vector2(textureRegion.X, textureRegion.Y),
+                        colour,
+                        new Vector2(textureRegion.X, textureRegion.Y)),
+                     0 // top left vertex
+                 );
+                yield return new TestCaseData(
+                    new VertexPositionColourTexture(
+                        new Vector2(textureRegion.X + textureRegion.Width, textureRegion.Y),
+                        colour,
+                        new Vector2(textureRegion.X + textureRegion.Width, textureRegion.Y)),
+                    1 // top right vertex
+                );
+                yield return new TestCaseData(
+                    new VertexPositionColourTexture(
+                        new Vector2(textureRegion.X + textureRegion.Width, textureRegion.Y + textureRegion.Height),
+                        colour,
+                        new Vector2(textureRegion.X + textureRegion.Width, textureRegion.Y + textureRegion.Height)),
+                    2 // bottom right
+                );
+                yield return new TestCaseData(
+                    new VertexPositionColourTexture(
+                        new Vector2(textureRegion.X, textureRegion.Y + textureRegion.Height),
+                        colour,
+                        new Vector2(textureRegion.X, textureRegion.Y + textureRegion.Height)),
+                    3 // bottom left
+                );
+            }
         }
     }
 }
--- src/XogoEngine/Graphics/Sprite.cs
+++ src/XogoEngine/Graphics/Sprite.cs
@@ -1,4 +1,6 @@
 using System;
+using OpenTK;
+using XogoEngine.OpenGL.Vertex;
 
 namespace XogoEngine.Graphics
 {
@@ -9,8 +11,15 @@ namespace XogoEngine.Graphics
         private int y;
         private int width;
         private int height;
+        private Colour4 colour;
+        private VertexPositionColourTexture[] vertices = new VertexPositionColourTexture[4];
 
         public Sprite(TextureRegion textureRegion, int x, int y)
+            : this(textureRegion, x, y, Colour4.White)
+        {
+        }
+
+        public Sprite(TextureRegion textureRegion, int x, int y, Colour4 colour)
         {
             if (textureRegion == null)
             {
@@ -21,12 +30,42 @@ namespace XogoEngine.Graphics
             this.y = y;
             this.width = textureRegion.Width;
             this.height = textureRegion.Height;
+            this.colour = colour;
+            InitialiseVertices();
         }
 
         public int X => x;
         public int Y => y;
         public int Width => width;
         public int Height => height;
+        public Colour4 Colour => colour;
         public TextureRegion TextureRegion => textureRegion;
+        internal VertexPositionColourTexture[] Vertices => vertices;
+
+        /* Will need to revisit once at the stage to render on screen
+        * Feel these will be incorrect as of now */
+        private void InitialiseVertices()
+        {
+            vertices[0] = new VertexPositionColourTexture(
+                new Vector2(textureRegion.X, textureRegion.Y),
+                new Vector4(colour.R, colour.G, colour.B, colour.A),
+                new Vector2(textureRegion.X, textureRegion.Y)
+            );// top left vertex
+            vertices[1] = new VertexPositionColourTexture(
+                new Vector2(textureRegion.X + width, textureRegion.Y),
+                new Vector4(colour.R, colour.G, colour.B, colour.A),
+                new Vector2(textureRegion.X + width, textureRegion.Y)
+            );// top right vertex
+            vertices[2] = new VertexPositionColourTexture(
+                new Vector2(textureRegion.X + width, textureRegion.Y + height),
+                new Vector4(colour.R, colour.G, colour.B, colour.A),
+                new Vector2(textureRegion.X + width, textureRegion.Y + height)
+            );// bottom right vertex
+            vertices[3] = new VertexPositionColourTexture(
+                new Vector2(textureRegion.X, textureRegion.Y + height),
+                new Vector4(colour.R, colour.G, colour.B, colour.A),
+                new Vector2(textureRegion.X, textureRegion.Y + height)
+            );// bottom left vertex
+        }
     }
 }
