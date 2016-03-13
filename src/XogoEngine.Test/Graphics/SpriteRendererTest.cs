71a57531688657ac1d38224a22f257ca06ad10b8
Richard Kirby <rk755@york.ac.uk>
2016-03-13 18:11:26 +0000

    Adding SpriteRenderer test fixture and class.

---
+++ src/XogoEngine.Test/Graphics/SpriteRendererTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using Shouldly;
+using System;
+using XogoEngine.Graphics;
+
+namespace XogoEngine.Test.OpenGL
+{
+    [TestFixture]
+    internal sealed class SpriteRendererTest
+    {
+        
+    }
+}
---
+++ src/XogoEngine/Graphics/SpriteRenderer.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace XogoEngine.Graphics
+{
+    internal class SpriteRenderer
+    {
+        
+    }
+}
