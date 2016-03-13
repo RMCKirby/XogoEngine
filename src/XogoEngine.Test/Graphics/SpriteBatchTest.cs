9395319d2cf33d86a111749e743f980739688c93
Richard Kirby <rk755@york.ac.uk>
2016-03-13 15:05:10 +0000

    Beginning SpriteBatch constructor.

--- src/XogoEngine.Test/Graphics/SpriteBatchTest.cs
+++ src/XogoEngine.Test/Graphics/SpriteBatchTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using Shouldly;
+using System;
 using XogoEngine.Graphics;
 
 namespace XogoEngine.Test.Graphics
@@ -7,6 +8,14 @@ namespace XogoEngine.Test.Graphics
     [TestFixture]
     internal sealed class SpriteBatchTest
     {
-        
+        private SpriteBatch spriteBatch;
+
+        [Test]
+        public void Constructor_ThrowsArgumentNullException_OnNullSpriteSheet()
+        {
+            SpriteSheet nullSheet = null;
+            Action construct = () => new SpriteBatch(nullSheet);
+            construct.ShouldThrow<ArgumentNullException>();
+        }
     }
 }
--- src/XogoEngine/Graphics/SpriteBatch.cs
+++ src/XogoEngine/Graphics/SpriteBatch.cs
@@ -1,7 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace XogoEngine.Graphics
 {
     public sealed class SpriteBatch
     {
-        
+        public SpriteBatch(SpriteSheet spriteSheet)
+        {
+            if (spriteSheet == null)
+            {
+                throw new ArgumentNullException(nameof(spriteSheet));
+            }
+        }
     }
 }
