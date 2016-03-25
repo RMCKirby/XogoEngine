6442a533485de44708c0610434aaa43f7a3351d6
Richard Kirby <rk755@york.ac.uk>
2016-03-25 16:57:19 +0000

    Real BufferAdapter in place.

--- src/XogoEngine.OpenGL/Adapters/BufferAdapter.cs
+++ src/XogoEngine.OpenGL/Adapters/BufferAdapter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL4;
@@ -18,6 +19,24 @@ namespace XogoEngine.OpenGL.Adapters
         {
             GraphicsContext.Assert();
             GL.BindBuffer(target, handle);
+        }
+
+        public void BufferData<T>(BufferTarget target, IntPtr size, T[] data, BufferUsageHint usageHint) where T : struct
+        {
+            GraphicsContext.Assert();
+            GL.BufferData(target, size, data, usageHint);
+        }
+
+        public void BufferSubData<T>(BufferTarget target, IntPtr offset, IntPtr size, T[] data) where T : struct
+        {
+            GraphicsContext.Assert();
+            GL.BufferSubData(target, offset, size, data);
+        }
+
+        public void DeleteBuffer(int handle)
+        {
+            GraphicsContext.Assert();
+            GL.DeleteBuffer(handle);
         }
     }
 }
