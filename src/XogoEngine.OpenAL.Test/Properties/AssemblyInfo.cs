fdf74dcfbf58be1caf7fdf64374195233978b403
Richard Kirby <rk755@york.ac.uk>
2016-04-25 16:00:30 +0100

    Move OpenAL stuff (albeit not much) to seperate project for consistency.

---
+++ src/XogoEngine.OpenAL.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("XogoEngine.OpenAL.Test")]
+[assembly: AssemblyDescription("XogoEngine's OpenAL Layer Unit Tests")]
+[assembly: AssemblyCopyright("Richard Kirby")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
---
+++ src/XogoEngine.OpenAL/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XogoEngine.OpenAL")]
+[assembly: AssemblyDescription("XogoEngine's OpenAL Layer")]
+[assembly: AssemblyCopyright("Richard Kirby")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
